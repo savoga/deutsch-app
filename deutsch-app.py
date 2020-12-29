f304fe829d918b56036f3a81ae4fcf86297ff633
savoga <savoure.gael@gmail.com>
2020-12-29 21:31:25 +0100

    add state session

--- deutsch-app.py
+++ deutsch-app.py
@@ -1,3 +1,10 @@
+'''
+// Important notes about Streamlit \\
+The framework doesn't handle event binding (i.e. a specific function to be
+called when a user clicks on a button)! When a button is pressed, the whole
+script is rerun.
+'''
+
 import pandas as pd
 from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
@@ -6,6 +13,9 @@ import os
 import pickle
 import streamlit as st
 
+import dataclasses
+from gamestate import persistent_game_state
+
 SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
 
 # here enter the id of your google sheet
@@ -13,6 +23,14 @@ SAMPLE_SPREADSHEET_ID_input = '1lMkLiPKYXT1qCDeLK1qRDF3yPMkCWJtHrJ1LTsHhlgA'
 SAMPLE_RANGE_NAME = 'A1:AA1000'
 COLUMN_VOC = 'Voc'
 COLUMN_TRANSLATION_VOC = 'Traduction voc'
+
+# decorator automatically does the __init__() and __repr__()
+@dataclasses.dataclass
+class GameState:
+    word: str = ''
+    translation: str = ''
+    game_number : int = 0
+    game_over: bool = False
 
 def loadSpreadsheet():
     global values_input, service
@@ -41,26 +59,31 @@ def loadSpreadsheet():
     if not values_input:
         print('No data found.')
 
-def randomWord(column_name):
+def randomWord(df, column_name):
     translation = None
     while(not translation or translation==''):
-        random_word = df_data[column_name].sample(n=1).values[0]
-        translation = df_data[df_data[column_name]==random_word]['Traduction ' + column_name.lower()].values[0]
+        random_word = df[column_name].sample(n=1).values[0]
+        translation = df[df[column_name]==random_word]['Traduction ' + column_name.lower()].values[0]
     return random_word, translation
 
-def isCorrect(user_answer, correct_answer):
-    if user_answer==correct_answer:
-        st.write('Correct!')
-    else:
-        st.write('Wrong!')
-
 loadSpreadsheet()
-df_data = pd.DataFrame(values_input[1:], columns=values_input[0])
-user_input = None
 
-if st.button(COLUMN_VOC):
-    random_word, translation = randomWord(COLUMN_VOC)
-    st.write(random_word)
-    user_input = st.text_input("Your answer")
-if user_input:
-    isCorrect(user_input, translation)
+df_data = pd.DataFrame(values_input[1:], columns=values_input[0])
+
+state = persistent_game_state(initial_state=GameState())
+
+if st.button('Générer un mot'):
+    state.word, state.translation = randomWord(df_data, COLUMN_VOC)
+    state.game_number += 1
+    state.game_over = False
+
+if not state.game_over:
+    guess = None
+    st.write(state.word)
+    guess = st.text_input("Votre traduction :", key=state.game_number)
+    if guess:
+        if guess != state.translation:
+            st.write("Mauvaise réponse !")
+        else:
+            st.write("Bonne réponse !")
+            state.game_over = True
